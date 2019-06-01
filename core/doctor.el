a1960d10ae046115f94c09cac4fec94bc11e3955
VentGrey <omarpurataf@gmail.com>
2019-06-01 01:08:22 -0500

    Partial translation to doctor

--- core/doctor.el
+++ core/doctor.el
@@ -13,25 +13,25 @@
   (let* ((path (expand-file-name file doom-cache-dir))
          (size (file-size path)))
     (when (and (numberp size) (> size 2000))
-      (warn! "%s is too large (%.02fmb). This may cause freezes or odd startup delays"
+      (warn! "%s es demasiado grande (%.02fmb). Esto puede causar lentitud o inicios muy tardados."
              (file-relative-name path doom-core-dir)
              (/ size 1024))
-      (explain! "Consider deleting it from your system (manually)"))))
+      (explain! "Considera eliminarle de tu sistema (manualmente)"))))
 
 (when (not (executable-find doom-projectile-fd-binary))
-  (warn! "Couldn't find the `fd' binary; project file searches will be slightly slower"))
+  (warn! "No se encontró el binario `fd'."))
 
 (let ((default-directory "~"))
   (require 'projectile)
   (when (cl-find-if #'projectile-file-exists-p projectile-project-root-files-bottom-up)
-    (warn! "Your $HOME is recognized as a project root")
-    (explain! "Doom will disable bottom-up root search, which may reduce the accuracy of project\n"
+    (warn! "Tu variable $HOME está siendo reconocida como la raíz del proyecto")
+    (explain! "Brann will disable bottom-up root search, which may reduce the accuracy of project\n"
               "detection.")))
 
 ;; There should only be one
 (when (and (file-equal-p doom-private-dir "~/.config/doom")
            (file-directory-p "~/.doom.d"))
-  (warn! "Both %S and '~/.doom.d' exist on your system"
+  (warn! "%S Ni '~/.doom.d' existen en tu sistema."
          (abbreviate-file-name doom-private-dir))
   (explain! "Doom will only load one of these (~/.config/doom takes precedence). Since\n"
             "it is rarely intentional that you have both, ~/.doom.d should be removed."))
